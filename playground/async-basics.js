74df42aaee5dd398ebd2b39bc3799621a3608083
Himanshu Negi <ecoworld007@gmail.com>
2018-04-20 15:22:02 +0530

    async basic using setTimeout

---
+++ playground/async-basics.js
@@ -0,0 +1,10 @@
+console.log('starting app');
+
+setTimeout(()=>{
+    console.log('inside of callback')
+},2000)
+
+setTimeout(()=>{
+    console.log('inside of 2nd callback')
+},0);
+console.log('finishing app');
