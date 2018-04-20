c6585f28c8b68fb90117ff17ae21a33878f3af21
Himanshu Negi <ecoworld007@gmail.com>
2018-04-20 16:48:53 +0530

    using JSON.stringify to pretty print body

--- app.js
+++ app.js
@@ -4,5 +4,5 @@ request({
     url: 'https://maps.googleapis.com/maps/api/geocode/json?address=house%20no.%2015%20sector%202c%20gurgaon%20haryana%20india',
     json: true
 },(error, response, body)=>{
-    console.log(body);
+    console.log(JSON.stringify(body,undefined,2));
 });
