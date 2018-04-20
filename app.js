5cb55de89cf6cbe21cff75780e5be7b335911f03
Himanshu Negi <ecoworld007@gmail.com>
2018-04-20 17:10:34 +0530

    using request and body object in the app.js

--- app.js
+++ app.js
@@ -4,5 +4,14 @@ request({
     url: 'https://maps.googleapis.com/maps/api/geocode/json?address=house%20no.%2015%20sector%202c%20gurgaon%20haryana%20india',
     json: true
 },(error, response, body)=>{
-    console.log(JSON.stringify(body,undefined,2));
+    if(error){
+        console.log(JSON.stringify(error,undefined,2));
+        return;
+    }
+    if(response.statusCode === 200 && body.status !== 'OVER_QUERY_LIMIT'){
+        console.log(`Address: ${body.results[0].formatted_address}`);
+        console.log(`Location: ${JSON.stringify(body.results[0].geometry.location,undefined,2)}`);
+    }else{
+        console.log('something bad happened');
+    }
 });
