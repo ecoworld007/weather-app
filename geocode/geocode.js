49c6c9db28592a68ee68264fcf56e7836e194abb
Himanshu Negi <ecoworld007@gmail.com>
2018-04-22 19:53:27 +0530

    modified to geocode file to replace callback with promises

--- geocode/geocode.js
+++ geocode/geocode.js
@@ -1,23 +1,25 @@
 const request = require('request');
-let geocodeAddress = (address, callback) => {
-    let encodedAddress = encodeURIComponent(address);
-    request({
-        url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}&key=${process.env.GOOGLE_MAP_API_KEY}`,
-        json: true
-    },(error, response, body)=>{
-        if(error){
-            callback('Unable to connect to server');
-        }else if(response.statusCode === 200 && body.status == 'OK'){
-            let result = {
-                address: body.results[0].formatted_address,
-                location: body.results[0].geometry.location
+let geocodeAddress = (address) => {
+    return new Promise((resolve, reject) => {
+        let encodedAddress = encodeURIComponent(address);
+        request({
+            url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}&key=${process.env.GOOGLE_MAP_API_KEY}`,
+            json: true
+        },(error, response, body)=>{
+            if(error){
+                reject('Unable to connect to server');
+            }else if(response.statusCode === 200 && body.status == 'OK'){
+                let result = {
+                    address: body.results[0].formatted_address,
+                    location: body.results[0].geometry.location
+                }
+                resolve(result);
+            }else if(body.status === 'ZERO_RESULTS'){
+                reject('No place found corressponding to the address provided');
+            }else{
+                reject(`Something bad happened with error message: ${body.status}`);
             }
-            callback(undefined, result);
-        }else if(body.status === 'ZERO_RESULTS'){
-            callback('No place found corressponding to the address provided');
-        }else{
-            callback(`Something bad happened with error message: ${body.status}`);
-        }
+        });
     });
 }
 
