471d38c1671752340e52decf7640e92456f5b4a7
Himanshu Negi <ecoworld007@gmail.com>
2018-04-22 16:28:27 +0530

    imporving error handling for response

--- app.js
+++ app.js
@@ -15,19 +15,20 @@ const argv = yargs.options({
 
 let encodedAddress = encodeURIComponent(argv.address);
 
-console.log(encodedAddress)
 request({
     url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
     json: true
 },(error, response, body)=>{
     if(error){
+        console.log('Unable to connect to server')
         console.log(JSON.stringify(error,undefined,2));
-        return;
-    }
-    if(response.statusCode === 200 && body.status !== 'OVER_QUERY_LIMIT'){
+    }else if(response.statusCode === 200 && body.status == 'OK'){
         console.log(`Address: ${body.results[0].formatted_address}`);
         console.log(`Location: ${JSON.stringify(body.results[0].geometry.location,undefined,2)}`);
+    }else if(body.status === 'ZERO_RESULTS'){
+        console.log('No place found corressponding to the address provided')
     }else{
         console.log('something bad happened');
+        console.log('Error message: ',body.status)
     }
 });
