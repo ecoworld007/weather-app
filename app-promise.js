528036feaad2acdbb3709ca1ce576bb062d3f391
Himanshu Negi <ecoworld007@gmail.com>
2018-04-23 01:49:22 +0530

    added functionality to fetch current address if no address is provided in CLI

--- app-promise.js
+++ app-promise.js
@@ -1,5 +1,8 @@
 const yargs = require('yargs');
 const axios = require('axios');
+const publicIp = require('public-ip')
+const geoip = require('geoip-lite')
+
 const argv = yargs.options({
     a:{
         demand : false,
@@ -8,31 +11,44 @@ const argv = yargs.options({
         string: true
     }
 })
-.default('address',"delhi")
 .help()
 .alias('help','h')
 .argv;
 
-let encodedAddress = encodeURIComponent(argv.address);
-let geocodeUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}&key=${process.env.GOOGLE_MAP_API_KEY}`;
+let getWeatherStat = (address) => {
+    console.log(address);
+    let encodedAddress = encodeURIComponent(address);
+    let geocodeUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}&key=${process.env.GOOGLE_MAP_API_KEY}`;
+    axios.get(geocodeUrl).then((response) => {
+        if(response.data.status === 'ZERO_RESULTS'){
+            throw new Error('Unable to find that address.');
+        }
+        console.log(response.data.results[0].formatted_address);
+        let lat = response.data.results[0].geometry.location.lat;
+        let lng = response.data.results[0].geometry.location.lng;
+        let weatherUrl = `https://api.darksky.net/forecast/${process.env.DARK_SKY_API_KEY}/${lat},${lng}`;
+        return axios.get(weatherUrl);
+    }).then((weatherResults) => {
+        console.log(`Currently its ${weatherResults.data.currently.temperature} but feels like ${weatherResults.data.currently.apparentTemperature} temperature.`);
+    },(error) => {
+        throw new Error('Unable to fetch temperature for that address');
+    }).catch((error) => {
+        if(error.code === 'ENOTFOUND'){
+            console.log('Unable to connect to API server');
+        }else{
+            console.log(error.message);
+        }
+    });
+}
 
-axios.get(geocodeUrl).then((response) => {
-    if(response.data.status === 'ZERO_RESULTS'){
-        throw new Error('Unable to find that address.');
-    }
-    console.log(response.data.results[0].formatted_address);
-    let lat = response.data.results[0].geometry.location.lat;
-    let lng = response.data.results[0].geometry.location.lng;
-    let weatherUrl = `https://api.darksky.net/forecast/${process.env.DARK_SKY_API_KEY}/${lat},${lng}`;
-    return axios.get(weatherUrl);
-}).then((weatherResults) => {
-    console.log(`Currently its ${weatherResults.data.currently.temperature} but feels like ${weatherResults.data.currently.apparentTemperature} temperature.`);
-},(error) => {
-    throw new Error('Unable to fetch temperature for that address');
-}).catch((error) => {
-    if(error.code === 'ENOTFOUND'){
-        console.log('Unable to connect to API server');
-    }else{
-        console.log(error.message);
-    }
-});
+if(argv.address){
+    //fetch temperature for address provided by CLI
+    getWeatherStat(argv.address);
+}else{
+    //checks for the ip address to find the city for which temperature is to be fetched
+    publicIp.v4().then(ip => {
+        return geoip.lookup(ip).city;
+    }).then((address) => {
+        getWeatherStat(address);
+    });
+}
--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const yargs = require('yargs');
+const publicIp = require('public-ip');
+const geoip = require('geoip-lite');
+
 const geocode = require('./geocode/geocode');
-const weather = require('./weather/weather')
+const weather = require('./weather/weather');
+
 const argv = yargs.options({
     a:{
         demand : false,
@@ -14,14 +18,28 @@ const argv = yargs.options({
 .alias('help','h')
 .argv;
 
-geocode.geocodeAddress(argv.address)
-.then((results) => {
-    console.log(results.address);
-    return  weather.getWeather(results.location.lat, results.location.lng);
-})
-.then((weatherResults) => {
-    console.log(`Currently its ${weatherResults.temperature} but feels like ${weatherResults.apparentTemperature} temperature.`);
-})
-.catch((error) => {
-    console.log(errormessage);
-});
+let getWeatherStat = (address) => {
+    geocode.geocodeAddress(address)
+    .then((results) => {
+        console.log(results.address);
+        return  weather.getWeather(results.location.lat, results.location.lng);
+    })
+    .then((weatherResults) => {
+        console.log(`Currently its ${weatherResults.temperature} but feels like ${weatherResults.apparentTemperature} temperature.`);
+    })
+    .catch((error) => {
+        console.log(errormessage);
+    });
+}
+
+if(argv.address){
+    //fetch temperature for address provided by CLI
+    getWeatherStat(argv.address);
+}else{
+    //checks for the ip address to find the city for which temperature is to be fetched
+    publicIp.v4().then(ip => {
+        return geoip.lookup(ip).city;
+    }).then((address) => {
+        getWeatherStat(address);
+    });
+}
